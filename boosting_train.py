29f1b3fe27bb971a4aa4ac236756e477bbb093cc
Daria <93913290+blondered@users.noreply.github.com>
2022-01-12 18:33:48 +0300

    Update boosting_train.py

--- boosting_train.py
+++ boosting_train.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import pickle
-from recsys_toolkit import *
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from catboost import CatBoostClassifier
