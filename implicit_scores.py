f1a09159d26b9d4263ef23ef01f27055068040bb
Daria <93913290+blondered@users.noreply.github.com>
2022-01-12 17:53:49 +0300

    Update implicit_scores.py

--- implicit_scores.py
+++ implicit_scores.py
@@ -63,7 +63,8 @@ def get_implicit_candidates(full_train, overall_known_items_mapped, warm_idx):
 
 users_df = pd.read_csv('data/users_processed.csv',)
 items_df = pd.read_csv('data/items_processed.csv',)
-interactions_df = pd.read_csv('data/interactions_processed.csv', parse_dates=['last_watch_dt'])
+interactions_df = pd.read_csv('data/interactions_processed.csv', 
+                              parse_dates=['last_watch_dt'])
 submission = pd.read_csv('data/sample_submission.csv')
 interactions_df.sort_values(by = 'last_watch_dt', inplace = True)
 
@@ -74,29 +75,42 @@ items_inv_mapping = dict(enumerate(interactions_df['item_id'].unique()))
 items_mapping = {v: k for k, v in items_inv_mapping.items()}
 
 # Preparing data for implicit scores for submit
-overall_known_items = interactions_df.groupby('user_id')['item_id'].apply(list).to_dict()
+overall_known_items = interactions_df.groupby('user_id')['item_id'].apply(
+    list).to_dict()
 overall_known_items_mapped = {}
 for user, recommend in overall_known_items.items():
-    overall_known_items_mapped[user] = list(map(lambda x: items_mapping[x], recommend))
+    overall_known_items_mapped[user] = list(map(lambda x: 
+                                                items_mapping[x], recommend))
 interactions_df['order_from_recent'] = interactions_df.sort_values(
       by = ['last_watch_dt'], ascending = False).groupby('user_id').cumcount() + 1
-warm_idx = np.intersect1d(interactions_df['user_id'].unique(), submission['user_id'].unique())
+warm_idx = np.intersect1d(interactions_df['user_id'].unique(), 
+                          submission['user_id'].unique())
 
 # Preparing data for implicit scores for boosting training
 last_date_df = interactions_df['last_watch_dt'].max()
 boosting_split_date = last_date_df - pd.Timedelta(days = 14)
-boosting_data = interactions_df[(interactions_df['last_watch_dt'] > boosting_split_date)].copy()  # все просмотры позже сплита
-before_boosting = interactions_df[(interactions_df['last_watch_dt'] <= boosting_split_date)].copy()
-before_boosting_known_items = before_boosting.groupby('user_id')['item_id'].apply(list).to_dict()
+boosting_data = interactions_df[(interactions_df['last_watch_dt'] > \
+                                 boosting_split_date)].copy() 
+before_boosting = interactions_df[(interactions_df['last_watch_dt'] <= \
+                                   boosting_split_date)].copy()
+before_boosting_known_items = before_boosting.groupby(
+    'user_id')['item_id'].apply(list).to_dict()
 before_boosting_known_items_mapped = {}
 for user, recommend in before_boosting_known_items.items():
-    before_boosting_known_items_mapped[user] = list(map(lambda x: items_mapping[x], recommend))
-before_boosting['order_from_recent'] = before_boosting.sort_values(by = ['last_watch_dt'], ascending = False).groupby('user_id').cumcount() + 1
-boost_warm_idx = np.intersect1d(before_boosting['user_id'].unique(), boosting_data['user_id'].unique())
+    before_boosting_known_items_mapped[user] = list(map(lambda x: 
+                                                        items_mapping[x], 
+                                                        recommend))
+before_boosting['order_from_recent'] = before_boosting.sort_values(
+    by = ['last_watch_dt'], ascending = False).groupby('user_id').cumcount() + 1
+boost_warm_idx = np.intersect1d(before_boosting['user_id'].unique(), 
+                                boosting_data['user_id'].unique())
 
 # Getting implicit scores and saving to csv
-impl_recs_for_submit =  get_implicit_candidates(interactions_df, overall_known_items_mapped, warm_idx)
+impl_recs_for_submit =  get_implicit_candidates(interactions_df, 
+                                                overall_known_items_mapped, warm_idx)
 impl_recs_for_submit.to_csv('data/impl_scores_for_submit.csv', index = False)
 
-impl_recs_for_boost_train =  get_implicit_candidates(before_boosting, before_boosting_known_items_mapped, boost_warm_idx)
+impl_recs_for_boost_train =  get_implicit_candidates(before_boosting, 
+                                                     before_boosting_known_items_mapped, 
+                                                     boost_warm_idx)
 impl_recs_for_boost_train.to_csv('data/impl_scores_for_boost_train.csv', index = False)
